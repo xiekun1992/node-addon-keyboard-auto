bdd0e0424e29a920d762af741bf86918165b7185
xiekun1992 <xkfront@outlook.com>
2020-04-02 01:20:18 +0800

    修改linux server下测试不执行

--- test/test.js
+++ test/test.js
@@ -1,26 +1,31 @@
 const keyboardAuto = require('../index')
 const {performance} = require('perf_hooks')
-
+const os = require('os')
 // console.log(keyboardAuto)
-setTimeout(() => {
 
-    const st = performance.now()
+if (os.platform() == 'linux' && !process.env.DISPLAY) {
+    console.log('npm test will not run on linux server') // 没有GUI环境导致SegmentFault错误
+} else {
+    setTimeout(() => {
     
-    keyboardAuto.keydown('a')
-    keyboardAuto.keyup('a')
-    keyboardAuto.keydown('a')
-    keyboardAuto.keyup('a')
-    keyboardAuto.keydown('a')
-    keyboardAuto.keyup('a')
-    keyboardAuto.keydown('a')
-    keyboardAuto.keyup('a')
-    keyboardAuto.keydown('a')
-    keyboardAuto.keyup('a')
-    keyboardAuto.keydown('controlleft')
-    keyboardAuto.keydown('v')
-    keyboardAuto.keyup('v')
-    keyboardAuto.keyup('controlleft')
+        const st = performance.now()
+        
+        keyboardAuto.keydown('a')
+        keyboardAuto.keyup('a')
+        keyboardAuto.keydown('a')
+        keyboardAuto.keyup('a')
+        keyboardAuto.keydown('a')
+        keyboardAuto.keyup('a')
+        keyboardAuto.keydown('a')
+        keyboardAuto.keyup('a')
+        keyboardAuto.keydown('a')
+        keyboardAuto.keyup('a')
+        keyboardAuto.keydown('controlleft')
+        keyboardAuto.keydown('v')
+        keyboardAuto.keyup('v')
+        keyboardAuto.keyup('controlleft')
+        
+        console.log(`${(performance.now() - st) / 10}ms`)
     
-    console.log(`${(performance.now() - st) / 10}ms`)
-
-}, 3000)    
+    }, 3000)    
+}
